d1b1c591cc8d3b6421b947299e1b0d4fcb4ba582
CrispyBaguette <alexandre.bruyant@gmail.com>
2019-11-16 10:12:25 +0000

    Setup headless chrome runner

--- projects/ngx-leaflet-fullscreen/karma.conf.js
+++ projects/ngx-leaflet-fullscreen/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,7 +28,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+            base: 'ChromeHeadless',
+            flags: [
+                '--no-sandbox', // required to run without privileges in docker
+                '--user-data-dir=/tmp/chrome-test-profile',
+                '--disable-web-security'
+            ]
+        }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
